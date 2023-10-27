7e2b13f13920c0e56e43eeeb0baabf7fc4cb2ced
jeondahoon <ekgns01913@naver.com>
2023-10-27 13:06:20 +0900

    수정 수정

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
+
+
+//저장 할공간 만들어서 다시 만들어보기
+
+
 int main()
 {
-	srand(time(0));
-	int random1= 1 + rand() % 52;
-	int random2= 1 + rand() % 52;
-	int random3= 1 + rand() % 52;
-	int random4= 1 + rand() % 52;
+	int pocket[52];
+	for (int i = 0; i < 52; i++)
 	{
-		printf("랜덤값 : % d\n", random1);
-		printf("랜덤값 : % d\n", random2);
-		printf("랜덤값 : % d\n", random3);
-		printf("랜덤값 : % d\n", random4);
+		pocket[i] = i + 1;
 	}
-
-
+	srand((unsigned int)time(nullptr));
+	for (int i = 0; i < 10000; i++)
+	{
+		int random1 = rand() % 52;
+		int random2 = rand() % 52;
+		int temp = pocket[random1];
+		pocket[random1] = pocket[random2];
+		pocket[random2] = temp;
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		cout << pocket[i] << endl;
+	}
 	return 0;
 }
